471354ebf3b2fa290c6b721f8896d5505140f2cc
Austen Constable <austen@crossfire.ltd.uk>
2018-10-07 22:22:24 +0100

    Added team name to dashboard

--- web/src/main/java/team/dashboard/web/metrics/DashboardController.java
+++ web/src/main/java/team/dashboard/web/metrics/DashboardController.java
@@ -1,18 +1,35 @@
 package team.dashboard.web.metrics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import team.dashboard.web.team.Team;
+import team.dashboard.web.team.TeamRepository;
+
+import java.util.Optional;
 
 @Controller
 public class DashboardController
     {
 
+    @Autowired
+    private TeamRepository teamRepository;
+
     @GetMapping("/dashboard/{teamId}/")
     public String graph(Model model, @PathVariable String teamId)
         {
+
+        Optional<Team> team = teamRepository.findByTeamId(teamId);
+        if (team.isPresent())
+            {
+            model.addAttribute("teamName", team.get().getTeamName());
+            model.addAttribute("platformName", team.get().getPlatformName());
+            model.addAttribute("domainName", team.get().getDomainName());
+            }
+
         model.addAttribute("team", teamId);
-        return "trendgraph";
+        return "dashboard";
         }
     }
