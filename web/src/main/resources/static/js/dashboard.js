df3c8c852a1fd1ecddb29698150bf40a73a50e60
Austen Constable <awconstable@gmail.com>
2019-03-24 23:36:22 +0000

    Started to bring in capture page

--- web/src/main/resources/static/js/dashboard.js
+++ web/src/main/resources/static/js/dashboard.js
@@ -1,13 +1,103 @@
 $(document).ready(function () {
     feather.replace();
 
+    loadGraphs();
+
+    $("#date").datepicker({
+        dateFormat: "yy-mm-dd"
+    });
+});
+
+function loadGraphs() {
     loadTrendData("/cycletime/", team).done(drawCycleTimeChart);
     loadTrendData("/deployment_frequency/", team).done(drawDeploymentFrequencyChart);
     loadTrendData("/incidents_due_to_change/", team).done(drawIncidentsDueToChangeChart);
     loadTrendData("/production_defects/", team).done(drawProductionDefectsChart);
     loadTrendData("/team_happiness/", team).done(drawTeamHappinessChart);
     loadTrendData("/customer_satisfaction/", team).done(drawCustomerSatisfactionChart);
+}
 
+var frm = $('#metric_capture');
+
+frm.submit(function (e) {
+
+    e.preventDefault();
+
+    $("#metric_capture input[type=text]").each(function () {
+        if (this.name === 'date' || this.value === "") {
+            console.log(this.name + ': skip field');
+            return;
+        }
+        $.ajax({
+            method: "GET",
+            url: "/metrics/" + this.name + "/" + team + "/" + $("#date").val() + "/" + this.value,
+            success: function (url) {
+                console.log(url + ': Submission was successful.');
+            },
+            error: function (url) {
+                console.log(url + ': An error occurred.');
+            }
+        });
+    });
+    $("#message").text("Metrics Submitted");
+    return false;
+
+});
+
+var captureLinkElem = $("#capture-link");
+var dashboardLinkElem = $("#dashboard-link");
+var captureContentElem = $("#capture-content");
+var dashboardContentElem = $("#dashboard-content");
+
+captureLinkElem.click(function () {
+    captureLinkElem.addClass("active");
+    dashboardLinkElem.removeClass("active");
+    captureContentElem.removeClass("d-none").addClass("d-block");
+    dashboardContentElem.removeClass("d-block").addClass("d-none");
+});
+
+dashboardLinkElem.click(function () {
+    loadGraphs();
+    captureLinkElem.removeClass("active");
+    dashboardLinkElem.addClass("active");
+    captureContentElem.addClass("d-none").removeClass("d-block");
+    dashboardContentElem.addClass("d-block").removeClass("d-none");
+});
+
+$("#refresh-button").click(function () {
+    loadGraphs();
+});
+
+$("#date").change(function () {
+    $("#metric_capture input[type=text]").each(function () {
+
+        var elementid = this.id;
+
+        if (this.name === 'date') {
+            console.log(this.name + ': skip field');
+            return;
+        } else {
+            $("#" + elementid).val("");
+        }
+
+        function success_cb(data) {
+            if (data !== "") {
+                console.log(elementid + " : " + data.value);
+                $("#" + elementid).val(data.value);
+            }
+        }
+
+        $.ajax({
+            method: "GET",
+            url: "/metrics/" + this.name + "/" + team + "/" + $("#date").val(),
+            success: function (data) {
+                success_cb(data);
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
+    });
 });
 
 function loadTrendData(url, slug) {
@@ -71,7 +161,7 @@ function drawIncidentsDueToChangeChart(data) {
 
     var ctx = $('#chart3');
 
-    new Chart(ctx, getChartConfig(data, "Incidents due to change", "Incidents"));
+    new Chart(ctx, getChartConfig(data, "Incidents Due To Change", "Incidents"));
 
 }
 
