a1aa2a7f52694c75df96eaabd19e5b139cdfd901
Austen Constable <awconstable@gmail.com>
2019-06-05 16:52:40 +0100

    Fixed failing controller test due to the full application context loading.

--- web/src/test/java/team/dashboard/web/metrics/TeamMetricsControllerTest.java
+++ web/src/test/java/team/dashboard/web/metrics/TeamMetricsControllerTest.java
@@ -4,12 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import team.dashboard.web.collection.TeamCollectionReportService;
+import team.dashboard.web.team.TeamRestRepository;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -21,8 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TeamMetricsController.class)
 public class TeamMetricsControllerTest
     {
     @Autowired
@@ -30,6 +30,12 @@ public class TeamMetricsControllerTest
 
     @MockBean
     private TeamMetricRepository mockTeamMetricRepository;
+
+    @MockBean
+    private TeamRestRepository teamRestRepository;
+
+    @MockBean
+    private TeamCollectionReportService teamCollectionReportService;
 
     private LocalDate now;
 
