acd4f7b5a0f0d85b3320467a038d7f9f14b8990c
Austen Constable <awconstable@gmail.com>
2021-06-09 11:46:03 +0200

    #7 Switch to using test-containers for MongoDB integration testing

--- web/src/test/java/team/dashboard/web/BaseMongoTest.java
+++ web/src/test/java/team/dashboard/web/BaseMongoTest.java
@@ -1,13 +1,40 @@
 package team.dashboard.web;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @DataMongoTest
-@TestPropertySource(properties = "app.scheduling.enable=false")
+@ContextConfiguration(initializers = {BaseMongoTest.Initializer.class})
 public class BaseMongoTest
     {
+
+    @ClassRule
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:3.6.21"));
+
+    @BeforeAll
+    static void setUpAll() {
+    mongoDBContainer
+        .withReuse(true)
+        .start();
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext>
+        {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext)
+            {
+            TestPropertyValues.of(
+                "spring.data.mongodb.uri=" + mongoDBContainer.getReplicaSetUrl()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+            }
+        }
     }
