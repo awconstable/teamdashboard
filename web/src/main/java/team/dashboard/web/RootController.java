a14e27fc0c1bc25b9d1bff7a503d1fed3575348b
Austen Constable <awconstable@gmail.com>
2021-07-09 13:20:05 +0200

    #86 hash version static resources. set no cache header on index. set all other static resources to cache for 365 days.

--- web/src/main/java/team/dashboard/web/RootController.java
+++ web/src/main/java/team/dashboard/web/RootController.java
@@ -1,16 +1,22 @@
 package team.dashboard.web;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class RootController
     {
 
     @GetMapping({"/", "/teamexplorer/**", "/dashboard/**", "/capture/**", "/collection/**", "/deployments/**", "/incidents/**", "/changerequests/**"})
-    public String index(Model model)
+    public String index(Model model, HttpServletResponse response)
         {
+        response.setHeader(HttpHeaders.CACHE_CONTROL,"no-cache, must-revalidate, no-store");
+        response.setHeader("Expires", "0");
+        
         return "dashboard";
         }
 
